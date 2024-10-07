321550ce8b73550d40e05255f33ebf4474039e6d
brissonwilliam <brissonwilliam1@hotmail.com>
2024-10-06 22:41:40 -0400

    :sparkles: it all works!

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,12 @@ fn main() {
     let args = MyArgs::parse();
     println!("INFO: provided path is {}", args.path);
 
-    let mut watch = watch::new(args.path.as_str());
+    let discord_url = match std::env::var("DISCORD_URL") {
+        Ok(var) => var,
+        Err(err) => panic!("DISCORD_URL env var must be set: {}", err),
+    };
+
+    let mut watch = watch::new(args.path.as_str(), &discord_url);
     watch.run().expect("error running mc notify log watch");
 }
 
